78e00dbe070b730c5e2ea1889c40a04a67186b4e
Abhinav <sharma.abhinav@ideametrics.co.in>
2014-08-04 09:35:17 +0000

    CHanged csproj file to correct error ms2**

--- Substract/Substract/Default.aspx.cs
+++ Substract/Substract/Default.aspx.cs
@@ -12,7 +12,7 @@ namespace Substract
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ,;
+            
         }
 
         protected void Button1_Click(object sender, EventArgs e)
